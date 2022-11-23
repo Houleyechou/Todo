4a98aba947c86636dc0e283bae6f4ea4e2e519da
Houleyechou <h.niang3@isepdiamniadio.edu.sn>
2022-11-23 11:33:56 +0000

    affichage Todo

---
+++ app.js
@@ -0,0 +1,33 @@
+//////var x = Number(prompt("entrer la premiere valeur"))  
+//var y = Number(prompt("entrer la deuxieme valeur"))
+//var s = function (a , b) {
+  //  return a + b 
+////var sous = function (a , b) {
+  //    return a - b 
+//}
+// var mult = function (a , b) {
+     // return a * b
+//}
+// var div = function  (a , b){
+     // return a / b}
+// if (z == 1) {
+        // var som = s (x,y)
+        // console.log(som)
+
+    //\    }
+// if(z == 2) { 
+         // var sou = sous(x,y)
+         // console.log(sou)
+//}
+// if(z == 3) { 
+    // var mul = mult (x,y)
+    // console.log(mul)
+
+//}
+// if(z == 4){ 
+    // var di = div (x,y)
+    // console.log(di)
+          
+//}//
+var x = Number(prompt("entrer la fonction")
+    if (true) {}
